8e5e950e8c1303e8fe5170cad0a4e04e963529b9
innpei <43521839+innpei@users.noreply.github.com>
2021-05-30 10:54:59 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
-"""General-purpose training script for image-to-image translation.
+"""Training script for CycleGAN.
 
-This script works for various models (with option '--model': e.g., pix2pix, cyclegan, colorization) and
+This script works for CycleGAN model and
 different datasets (with option '--dataset_mode': e.g., aligned, unaligned, single, colorization).
 You need to specify the dataset ('--dataroot'), experiment name ('--name'), and model ('--model').
 
@@ -11,13 +11,8 @@ The script supports continue/resume training. Use '--continue_train' to resume y
 Example:
     Train a CycleGAN model:
         python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan
-    Train a pix2pix model:
-        python train.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA
-
-See options/base_options.py and options/train_options.py for more training options.
-See training and test tips at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/tips.md
-See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/qa.md
 """
+
 import time
 from options.train_options import TrainOptions
 from data import create_dataset
