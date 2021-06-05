efabf4ba6731ff795c0157bdb1d5ebbfb376bc40
innpei <43521839+innpei@users.noreply.github.com>
2021-06-05 11:47:43 +0300

    Create download_dataset.sh

---
+++ script/download_dataset.sh
@@ -0,0 +1,10 @@
+FILE=$1
+
+echo "Specified [$FILE]"
+URL=https://drive.google.com/drive/folders/1NcJR493-wzJAaBnQKVdnZDvbF1iB9_Tm?usp=sharing
+ZIP_FILE=./datasets/$FILE.zip
+TARGET_DIR=./datasets/$FILE/
+wget -N $URL -O $ZIP_FILE
+mkdir $TARGET_DIR
+unzip $ZIP_FILE -d ./datasets/
+rm $ZIP_FILE
