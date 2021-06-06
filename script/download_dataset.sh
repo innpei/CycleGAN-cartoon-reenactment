7f607573b788b02d8cfe0b4bc8ec08108254e38d
innpei <43521839+innpei@users.noreply.github.com>
2021-06-06 17:42:09 +0300

    Update download_dataset.sh

--- script/download_dataset.sh
+++ script/download_dataset.sh
@@ -1,6 +1,7 @@
 FILE=$1
 
 echo "Specified [$FILE]"
+mkdir datasets
 URL=https:https://docs.google.com/uc?export=download&id=140D-RG8JWPGKL_aQ98E100NIRyT5TZNW
 TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $ZIP_FILE
