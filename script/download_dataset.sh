48e2469b9d23cf8dae9f135794e02be37fa1f911
innpei <43521839+innpei@users.noreply.github.com>
2021-06-06 18:46:40 +0300

    Update download_dataset.sh

--- script/download_dataset.sh
+++ script/download_dataset.sh
@@ -2,7 +2,7 @@ FILE=$1
 
 echo "Specified [$FILE]"
 mkdir datasets
-URL=https://drive.google.com/file/d/140D-RG8JWPGKL_aQ98E100NIRyT5TZNW/view?usp=sharing/Ilyas_Jack.zip
+URL=https://drive.google.com/file/d/140D-RG8JWPGKL_aQ98E100NIRyT5TZNW/view?usp=sharing
 ZIP_FILE=./datasets/$FILE.zip
 TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $ZIP_FILE
